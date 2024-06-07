ae9c39b29fbd3ff0ac731f6a882334f5a271de31
Amr Khaled Akl <aklamr2003@gmail.com>
2024-06-08 00:09:14 +0300

    Start project

---
+++ Start/Start/DDA.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Start
+{
+    internal class DDA
+    {
+        public float X, Y;
+        float dy, dx, m;
+        public float cx, cy;
+        int speed = 10;
+        public void calc(DDA End)
+        {
+            dy = End.Y - Y;
+            dx = End.X - X;
+            m = dy / dx;
+            cx = X;
+            cy = Y;
+        }
+        public bool CalcNextPoint(DDA End)
+        {
+            if (Math.Abs(dx) > Math.Abs(dy))
+            {
+                if (X < End.X)
+                {
+                    cx += speed;
+                    cy += m * speed;
+                    if (cx >= End.X)
+                    {
+                        return false;
+                    }
+
+                }
+                else
+                {
+                    cx -= speed;
+                    cy -= m * speed;
+                    if (cx <= End.X)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                if (Y < End.Y)
+                {
+                    cy += speed;
+                    cx += 1 / m * speed;
+                    if (cy >= End.Y)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    cy -= speed;
+                    cx -= 1 / m * speed;
+                    if (cy <= End.Y)
+                    {
+                        return false;
+                    }
+                }
+
+            }
+            return true;
+        }
+
+    }
+}
---
+++ Start/Start/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Start
+{
+    public partial class Form1 : Form
+    {
+        Bitmap off;
+        Timer T = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(Form1_Paint);
+            this.Load += new EventHandler(Form1_Load);
+            T.Start();
+            T.Tick += new EventHandler(T_Tick);
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        void T_Tick(object sender, EventArgs e)
+        {
+            DrawDubb(this.CreateGraphics());
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            off = new Bitmap(ClientSize.Width, ClientSize.Height);
+        }
+
+        void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawDubb(e.Graphics);
+        }
+        void DrawScene(Graphics g)
+        {
+            g.Clear(Color.White);
+        }
+
+        void DrawDubb(Graphics g)
+        {
+            Graphics g2 = Graphics.FromImage(off);
+            DrawScene(g2);
+            g.DrawImage(off, 0, 0);
+        }
+    }
+}
+
